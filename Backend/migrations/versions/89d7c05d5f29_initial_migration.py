6967b2898cb09c0ec76199fcf41cded2c4005a6d
Njokimary <marynjoki738@gmail.com>
2023-10-31 15:20:21 +0300

    feat:migrations upgrade

--- Backend/migrations/versions/89d7c05d5f29_initial_migration.py
+++ Backend/migrations/versions/89d7c05d5f29_initial_migration.py
@@ -1,8 +1,8 @@
-"""initial migration
+"""Initial migration
 
-Revision ID: 6bf02ccd7763
+Revision ID: 89d7c05d5f29
 Revises: 
-Create Date: 2023-10-31 13:16:52.092913
+Create Date: 2023-10-31 15:03:47.224787
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '6bf02ccd7763'
+revision = '89d7c05d5f29'
 down_revision = None
 branch_labels = None
 depends_on = None
